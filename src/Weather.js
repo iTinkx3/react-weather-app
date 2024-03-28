3494bb126362cce7db9bb6ec8a37f78067887fbc
iTinkx3 <samantha.collins@live.com.au>
2024-03-28 21:29:45 +0000

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Weather.css";
+import "./App.css"
 
 export default function Weather() {
     return (
@@ -8,10 +9,10 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                <input type="search" placeholder="Enter a city..." className="form-control" />
+                <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
                     </div>
                     <div className="col-3">
-                <input type="submit" value="search" className="btn btn-primary" />
+                <input type="submit" value="search" className="btn btn-primary w-100" />
                     </div>
                 </div>
             </form>
@@ -20,10 +21,15 @@ export default function Weather() {
                 <li>Wednesday 07:00</li>
                 <li>Light Rain</li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png" alt="Light Rain" />
-                    6°C
+                    <div className="clearfix">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png" alt="Light Rain" className="float-left" />
+                    <div className="float-left">
+                        <span className="temperature">6</span>
+                        <span className="unit">°C</span>
+                    </div>
+                    </div>
                 </div>
                 <div className="col-6">
                     <ul>
