547ef2d38c1b2a034f04321213ebc2a704e1684b
iTinkx3 <samantha.collins@live.com.au>
2024-03-28 20:09:18 +0000

    Added html

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-        <h1>Weather Apps</h1>
+      <div className="container">
+        <Weather />
         <footer>
           This project was coded by <a href="https://magenta-khapse-6f3ded.netlify.app" target="_blank">Sam Collins</a> and is{" "}
         <a href="https://github.com/iTinkx3/react-weather-app" target="_blank">open-sourced on GitHub</a>
         </footer>
+        </div>
     </div>
   );
 }
